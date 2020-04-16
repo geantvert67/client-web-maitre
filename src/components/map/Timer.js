f1d24124dcb42032b04c51f4fda508870ff68e9e
Clément KOENIG <clement.koenig@unistra.fr>
2020-04-16 14:10:13 +0200

    Timer

---
+++ src/components/map/Timer.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { secondsToDuration } from '../../utils/utils';
+import { Row, Col } from 'react-bootstrap';
+
+function Timer({ duration, launchedAt }) {
+    const [time, setTime] = useState(null);
+
+    useEffect(() => {
+        const i = setInterval(() => setTime(getTime()), 1000);
+        return () => clearInterval(i);
+    }, []);
+
+    const getTime = () => {
+        return Math.floor(
+            duration -
+                moment.duration(moment().diff(moment(launchedAt))).asSeconds()
+        );
+    };
+
+    return time ? (
+        <Row className="btn-toast-left px-1 align-items-center">
+            <Col xs="auto" className="pr-2">
+                <FontAwesomeIcon icon={faClock} size="lg" />
+            </Col>
+            <Col xs="auto" className="pl-2">
+                <h6 className="mb-0">{secondsToDuration(time)}</h6>
+            </Col>
+        </Row>
+    ) : (
+        <></>
+    );
+}
+
+export default Timer;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,7 +3,7 @@ export const secondsToDuration = (seconds) => {
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor((seconds % 3600) % 60);
 
-    return `${h === 0 ? '00' : ('0' + h).slice(-2)}:${
-        m === 0 ? '00' : ('0' + m).slice(-2)
-    }:${s === 0 ? '00' : ('0' + s).slice(-2)}`;
+    return `${h <= 0 ? '00' : ('0' + h).slice(-2)}:${
+        m <= 0 ? '00' : ('0' + m).slice(-2)
+    }:${s <= 0 ? '00' : ('0' + s).slice(-2)}`;
 };
