0050a95918a3c3297f9351bf9b4cebc7a2f9ccd8
Clément KOENIG <clement.koenig@unistra.fr>
2020-04-22 17:44:02 +0200

    Center on game area

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+import { Button, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVectorSquare } from '@fortawesome/free-solid-svg-icons';
 import { getCenterOfBounds } from 'geolib';
+import Score from './Score';
 import { useSocket } from '../../utils/useSocket';
 import { useGameAreas } from '../../utils/useGameAreas';
 import GameArea from './GameArea';
@@ -23,12 +27,14 @@ function Map() {
     const { socket } = useSocket();
     const [zoom, setZoom] = useState(17);
     const [position, setPosition] = useState(null);
+    const [showScore, setShowScore] = useState(false);
     const { gameAreas, setGameAreas } = useGameAreas();
     const { forbiddenAreas, setForbiddenAreas } = useForbiddenAreas();
     const { flags, setFlags, deleteFlag } = useFlags();
     const { markers, setMarkers } = useMarkers();
     const { setTeams } = useTeams();
     const { items, setItems, deleteItem } = useItems();
+    const map = useRef(null);
 
     useEffect(() => {
         socket.on('getAreas', (a) => {
@@ -86,38 +92,65 @@ function Map() {
         });
     };
 
+    const centerOnGameArea = () => {
+        map.current.leafletElement.panTo(
+            deserializePoint(getCenterOfBounds(gameAreas[0].coordinates[0]))
+        );
+    };
+
     return (
-        <LeafletMap
-            className="map"
-            center={position || [47.736544, 7.286776]}
-            zoom={zoom}
-            minZoom={5}
-            maxZoom={25}
-        >
-            <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+        <>
+            <LeafletMap
+                ref={map}
+                className="map"
+                center={position || [47.736544, 7.286776]}
+                zoom={zoom}
+                minZoom={5}
+                maxZoom={25}
+            >
+                <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
 
-            {gameAreas.map((area) => (
-                <GameArea key={area.id} area={area} />
-            ))}
+                {gameAreas.map((area) => (
+                    <GameArea key={area.id} area={area} />
+                ))}
 
-            {forbiddenAreas.map((area) => (
-                <ForbiddenArea key={area.id} area={area} />
-            ))}
+                {forbiddenAreas.map((area) => (
+                    <ForbiddenArea key={area.id} area={area} />
+                ))}
 
-            <PlayerList />
+                <PlayerList />
 
-            {flags.map((flag) => (
-                <FlagMarker key={flag.id} flag={flag} />
-            ))}
+                {flags.map((flag) => (
+                    <FlagMarker key={flag.id} flag={flag} />
+                ))}
 
-            {markers.map((marker) => (
-                <MarkerMarker key={marker.id} marker={marker} />
-            ))}
+                {markers.map((marker) => (
+                    <MarkerMarker key={marker.id} marker={marker} />
+                ))}
 
-            {items.map((item) => (
-                <ItemMarker key={item.id} item={item} />
-            ))}
-        </LeafletMap>
+                {items.map((item) => (
+                    <ItemMarker key={item.id} item={item} />
+                ))}
+            </LeafletMap>
+
+            <Score showScore={showScore} setShowScore={setShowScore} />
+
+            <Row className="btn-toast">
+                <Col xs="auto">
+                    <Button onClick={centerOnGameArea} variant="light">
+                        <FontAwesomeIcon icon={faVectorSquare} />
+                    </Button>
+                </Col>
+                <Col xs="auto">
+                    <Button
+                        onClick={() => setShowScore(true)}
+                        variant="success"
+                    >
+                        Score
+                    </Button>
+                </Col>
+            </Row>
+        </>
     );
 }
 
